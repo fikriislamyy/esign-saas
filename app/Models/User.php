985b9c3094d956c6bbb5873f523e7206dd2b016b
fikriislamyy <fikriislamyy@gmail.com>
2026-06-10 14:43:16 +0700

    Setup infrastructure

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
 }
---
+++ resources/js/Pages/Landing.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col items-center justify-center">
+        <h1 class="text-5xl font-bold">ESign SaaS</h1>
+
+        <p class="mt-4 text-gray-500">Secure Digital Signing Platform</p>
+
+        <div class="mt-8 flex gap-4">
+            <Link
+                href="/login"
+                class="px-4 py-2 bg-blue-600 text-white rounded"
+            >
+                Login
+            </Link>
+
+            <Link href="/register" class="px-4 py-2 border rounded">
+                Register
+            </Link>
+        </div>
+    </div>
+</template>
